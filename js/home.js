2ec00afc5d4c799573a9b4b000295f4ec935ea9e
jagp2097 <jagp2097@gmail.com>
2021-07-25 21:00:14 -0500

    se añadio bootstrap y avances en responsive

---
+++ js/home.js
@@ -0,0 +1,9 @@
+const sidebar = document.getElementById("sidebar");
+
+// sidebar.addEventListener("mousemove", () => {
+//   console.log("Por encima...");
+//   const sidebarItems = document.querySelectorAll(".sidebar-nav__link-text");
+//   sidebarItems.forEach(item => {
+//     item.style.display = "inline";
+//   });
+// });
